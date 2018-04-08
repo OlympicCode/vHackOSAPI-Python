936dd083157efd30a31b35bdf950c35f8d18c731
Gond0r <Gond0r@users.noreply.github.com>
2018-04-08 17:47:25 +0200

    Update update.py

--- update.py
+++ update.py
@@ -18,14 +18,14 @@ class Update():
 
         
         # get applications and update this
+        # check number of running tasks before the start of the for loop
+        # this value is stored in the variable update and should be incremented with every successfull update.
+        # This will reduce the number of requests sent to the server which will lower chances of botting detection.
+        getTask = self.ut.requestString("tasks.php", accesstoken=self.Configuration["accessToken"])
+        if 'updateCount' in getTask.keys():
+            update = getTask['updateCount']
+        
         for applications in self.store["apps"]:
-            getTask = self.ut.requestString("tasks.php",
-                                            accesstoken=self.Configuration["accessToken"])
-            try:
-            	update = len(getTask["updates"])
-            except KeyError:
-            	update = 0
-
             if update < 10:
                 Appid = int(applications["appid"])
                 for list_update in self.Configuration["update"]:
@@ -37,8 +37,10 @@ class Update():
                                                                accesstoken=self.Configuration["accessToken"],
                                                                appcode=application_update,
                                                                action="100")
-                            self.ut.viewsPrint("showMsgUpdate", "[{}] - Update for your {} +1".format(os.path.basename(__file__), list_update))
-                            time.sleep(0.5)
+                            if result['result'] == '0':
+                                self.ut.viewsPrint("showMsgUpdate", "[{}] - Update for your {} +1".format(os.path.basename(__file__), list_update))
+                                update = update + 1
+                                time.sleep(0.5)
                         else:
                             self.ut.viewsPrint("showMsgUpdate", "[{}] - you have not money to upgrade {}".format(os.path.basename(__file__), list_update))
                             time.sleep(0.5)
